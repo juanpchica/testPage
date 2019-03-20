78022cf7cb0b81b55cb8f8fb2abe0116d765907a
juanpchcia <juanpchica@hotmail.com>
2019-03-19 22:36:04 -0500

    Se agrega header en mobile y desktop

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 jQuery(document).ready(function($) {
-	
-	/* Menu */
+
+	// Menu Mobile
 	$('nav#menu').mmenu();
 	
-
+	// Show Searcher in mobile
+	$('#btn-searcher').click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('btn-searcher-active');
+		$('.content-searcher').toggleClass('content-searcher-show');
+	});
 });
