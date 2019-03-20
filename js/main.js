74fe8e388ffe27097ea96a9110b6ea943c977703
juanpchcia <juanpchica@hotmail.com>
2019-03-19 23:28:33 -0500

    Se agrega banner y servicios

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ jQuery(document).ready(function($) {
 		$(this).toggleClass('btn-searcher-active');
 		$('.content-searcher').toggleClass('content-searcher-show');
 	});
+
+	/* Sliders */
+	$('.slider-home').slick({
+		autoplay: true,
+ 	 	autoplaySpeed: 2000,
+ 	 	dots:true,
+ 	 	fade:false,
+ 	 	prevArrow: '<span class="arrow-left arrow-slick"><img src="./img/arrow-left.png"></span>',
+ 	 	nextArrow: '<span class="arrow-right arrow-slick"><img src="./img/arrow-right.png"></span>'
+	});
 });
