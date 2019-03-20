74ceb3241d26406fb263d9dd46d2e36a87c46110
juanpchcia <juanpchica@hotmail.com>
2019-03-19 23:58:34 -0500

    Se agrega sección de productos

--- js/main.js
+++ js/main.js
@@ -19,4 +19,38 @@ jQuery(document).ready(function($) {
  	 	prevArrow: '<span class="arrow-left arrow-slick"><img src="./img/arrow-left.png"></span>',
  	 	nextArrow: '<span class="arrow-right arrow-slick"><img src="./img/arrow-right.png"></span>'
 	});
+
+	$('.slider-products').slick({
+		autoplay: false,
+ 	 	autoplaySpeed: 2000,
+ 	 	dots:false,
+ 	 	fade:false,
+ 	 	prevArrow: '<span class="arrow-left arrow-slick"><img src="./img/arrow-left.png"></span>',
+ 	 	nextArrow: '<span class="arrow-right arrow-slick"><img src="./img/arrow-right.png"></span>',
+ 	 	slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+			    slidesToShow: 3,
+			    slidesToScroll: 3
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+			    slidesToShow: 2,
+			    slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1
+			  }
+			}
+		]
+	});
 });
