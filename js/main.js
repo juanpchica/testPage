29c75629c824aa04578cddbd04e3148f837efa31
juanpchcia <juanpchica@hotmail.com>
2019-03-20 20:30:52 -0500

    Se agrega about us

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function($) {
 	});
 
 	$('.slider-products').slick({
-		autoplay: false,
+		autoplay: true,
  	 	autoplaySpeed: 2000,
  	 	dots:false,
  	 	fade:false,
@@ -29,6 +29,41 @@ jQuery(document).ready(function($) {
  	 	nextArrow: '<span class="arrow-right arrow-slick"><img src="./img/arrow-right.png"></span>',
  	 	slidesToShow: 4,
 		slidesToScroll: 4,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+			    slidesToShow: 3,
+			    slidesToScroll: 3
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+			    slidesToShow: 2,
+			    slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1
+			  }
+			}
+		]
+	});
+
+
+	$('.slider-partners').slick({
+		autoplay: false,
+ 	 	autoplaySpeed: 2000,
+ 	 	dots:false,
+ 	 	fade:false,
+ 	 	prevArrow: '<span class="arrow-left arrow-slick"><img src="./img/arrow-left.png"></span>',
+ 	 	nextArrow: '<span class="arrow-right arrow-slick"><img src="./img/arrow-right.png"></span>',
+ 	 	slidesToShow: 5,
+		slidesToScroll: 5,
 		responsive: [
 			{
 			  breakpoint: 1024,
